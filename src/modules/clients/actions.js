3cdbddb9964c1ba530e8b35552e5691e91899a95
Anastasia Shapovalova <shabalinastya@gmail.com>
2019-01-19 10:55:02 +0000

    HW pizza is ready

--- src/modules/clients/actions.js
+++ src/modules/clients/actions.js
@@ -2,5 +2,5 @@ export const CREATE_NEW_ORDER = 'CREATE_NEW_ORDER';
 
 export const createNewOrder = (id, recipe) => ({
   type: CREATE_NEW_ORDER,
-  payload: { id, recipe }
+  payload: { id, recipe } 
 });
--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -7,8 +7,68 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
+const orderStep = ['clients', 'conveyor_1', 'conveyor_2', 'conveyor_3', 'conveyor_4', 'finish']
+
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER:      
+      return [
+        ...state,
+        {id:state.length+1, recipe: action.payload.recipe, ingredients:[], position: "clients"}
+      ]
+      case MOVE_ORDER_NEXT:
+        const updatedNextOrders = state.map(order => {
+          if(parseInt(order.id) === action.payload){
+            var newPosition = order.position
+            if (order.position === 'conveyor_4' && order.ingredients.length === order.recipe.length){
+              newPosition = 'finish'
+            } else {
+              for (var i=0; i< orderStep.length-2; i++){
+                if (orderStep[i] === order.position){
+                  newPosition = orderStep[i+1]
+                  break
+                }                  
+              }
+            }  
+            return { ...order, position: newPosition }
+          }
+          return order
+        })
+    return updatedNextOrders
+      case MOVE_ORDER_BACK:
+        const updatedPrevOrders = state.map(order => {
+          if(parseInt(order.id) === action.payload){
+            var newPosition = order.position
+            for (var i=2; i< orderStep.length; i++){
+                if (orderStep[i] === order.position){
+                  newPosition = orderStep[i-1]
+                  break
+                }
+            }
+            return { ...order, position: newPosition }
+          }
+          return order
+        })
+        return updatedPrevOrders
+      case ADD_INGREDIENT:
+        var actviveOrder = 0
+        console.log('here')
+        for (var i=0; i< state.length; i++){
+          if (state[i].position === action.payload.from ){
+            actviveOrder = state[i].id
+            break
+          }
+        }        
+        const updatedIngredOrders = state.map(order => {
+
+              if (parseInt(order.id) === actviveOrder 
+                  && order.recipe.includes(action.payload.ingredient)
+                  && !order.ingredients.includes(action.payload.ingredient)){
+                    return { ...order, ingredients: [...order.ingredients, action.payload.ingredient] }
+                  }
+            return order
+          })
+        return updatedIngredOrders
     default:
       return state;
   }
